1b9c87961131f131e0a83ce40147a698c9c93508
Arsen <76063924+Arsen3001@users.noreply.github.com>
2023-02-17 16:23:29 +0200

    Add files via upload

---
+++ force-app/main/default/aura/PopularBooks/PopularBooksHelper.js
@@ -0,0 +1,23 @@
+//test push
+({
+    getPopularBooks : function(component, event, helper) {    
+        let action = component.get("c.getPopularBooks");
+        action.setCallback(this, function(response) {
+            let state = response.getState();
+            if (state === "SUCCESS") {
+                component.set("v.data", response.getReturnValue());
+            } else if (state === "ERROR") {
+                let errors = response.getError();
+                if (errors) {
+                    if (errors[0] && errors[0].message) {
+                        console.log("Error message: " + 
+                                 errors[0].message);
+                    }
+                } else {
+                    console.log("Unknown error");
+                }
+            }
+        })
+        $A.enqueueAction(action);
+    }
+})
